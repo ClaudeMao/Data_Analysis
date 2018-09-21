4914df6dfcd1e7d917935ad62738c8bcb49a0fae
claudemao <claudemao0131@gmail.com>
2018-09-21 11:17:27 +0100

    polish errorbar

--- Experiments/official_fit.py
+++ Experiments/official_fit.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from scipy.stats import linregress
 from scipy.optimize import curve_fit #another way to fit curve
 
-filename = 'U:/Data_git/Data_Analysis/Experiments/raw_data.txt'
+filename = 'C:/Users/98415/Data_Analysis/Experiments/raw_data.txt'
 [distance, mean_time] = np.loadtxt(filename, delimiter=' ', unpack=True)
 print(distance)
 print(mean_time)
@@ -14,10 +14,11 @@ x = 0.5*mean_time**2
 slope, intercept, r_value, p_value, std_err = linregress(x,y)
 #Fit x and y Data using a linear regression
 
-errorname = 'U:/Data_git/Data_Analysis/Experiments/error_data.txt'
+errorname = 'C:/Users/98415/Data_Analysis/Experiments/error_data.txt'
 [dt, dl] = np.loadtxt(errorname, delimiter=' ', unpack=True)
 dt = 0.5*dt**2
 
+print(std_err)
 print('Slope, Error, Intercept, Correlation Coefficient:')
 print(slope, std_err, intercept, r_value**2)
 print('g = {} +- {} m/s^2'.format(slope, std_err))
