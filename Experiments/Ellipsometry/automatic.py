c8bf7b51cfcf8f8c2bf07601b5b1cfba8dc42814
claudemao <claudemao0131@gmail.com>
2019-02-21 23:00:38 +0000

    automatic plotting bot

--- Experiments/Ellipsometry/automatic.py
+++ Experiments/Ellipsometry/automatic.py
@@ -1,65 +1,117 @@
 import numpy as np
 import pandas as pd
+import os, sys
 import matplotlib.pyplot as plt
 from scipy.stats import linregress
 
-filename = input('Please input name of your worksheet: ')
-worksheet = pd.read_excel('filename')
-df = pd.DataFrame(worksheet)
+#this module could be used for two columns
+#splitting into two linear regression is supported
 
-def paint():
-	name_x = df.iloc[1:2, 1:2]
-	x = df[name_x]
-	name_y = df.iloc[1:2, ]
+def main():
+    current_path_of_py = os.path.dirname(os.path.abspath(sys.argv[0]))
+    os.chdir(current_path_of_py)
 
-	
-	
-[LHS, RHS] = np.loadtxt(filename, delimiter=',', unpack = True) # LHS = h, RHS = Vx^2
+    filename = input('Please input name of your worksheet(with type): ')
+    worksheet = pd.read_excel(filename)
+    df = pd.DataFrame(worksheet)
 
-y = LHS  
-x = RHS
-slope, intercept, r_value, p_value, std_err = linregress(x,y)
-#Fit x and y Data using a linear regression
-
-a = RHS[-1]
-xfit = np.linspace(0, a, 100)
-intercept = 0 # we set intercept to zero as we need
-yfit = slope*xfit + intercept
-plt.plot(xfit, yfit, 'r-', label= 'Linear Fit')
-plt.errorbar(x,y,fmt='o',label='Data point', xerr = 0, yerr = std_err, capsize = 3) 
-# we don't want error on x axis
-# to make horizontal line longer, we need to set parameter 'capsize' with appropriate value
-plt.grid()
-plt.ylabel('h[m]')
-plt.xlabel('Vx^2[m^2/s^2]')
-plt.legend(loc = 'best')
-print('Slope, Error, Intercept, Correlation Coefficient:') # r^2 = coefficient of determination
-print(slope, std_err, intercept, r_value**2)
-if num == 1: 
-    print('standard I in 2-1 = {}'.format(0.001550846))
-    I_exp = 2*0.051**2*(1.1925*9.8*slope-1.1925/2)
-    print('I in exp = {}'.format(I_exp))
-    plus = 2*0.051**2*(1.1925*9.8*(slope + std_err)-1.1925/2)
-    print('Error of I in exp = {}'.format(plus - I_exp))
-    minus = 2*0.051**2*(1.1925*9.8*(slope - std_err)-1.1925/2)
-    print('Error of I in exp = {}'.format(I_exp - minus))
-    print('I = {} +- {}'.format(I_exp, plus - I_exp))
-elif num == 2: 
-    print('standard I in 2-2 = {}'.format(0.000586376))
-    I_exp = 2*0.0335**2*(1.045*9.8*slope-1.045/2)
-    print('I in exp = {}'.format(I_exp)) 
-    plus = 2*0.0335**2*(1.045*9.8*(slope + std_err)-1.045/2)
-    print('Error of I in exp = {}'.format(plus - I_exp))
-    minus = 2*0.0335**2*(1.045*9.8*(slope - std_err)-1.045/2)
-    print('Error of I in exp = {}'.format(I_exp - minus))
-    print('I = {} +- {}'.format(I_exp, plus - I_exp))
-elif num == 3:
-    print('standard I in 2-3 = {}'.format(0.003133555))
-    I_exp = 2*0.051**2*(2.4095*9.8*slope-2.4095/2)
-    print('I in exp = {}'.format(I_exp))
-    plus = 2*0.051**2*(2.4095*9.8*(slope + std_err)-2.4095/2)
-    print('Error of I in exp = {}'.format(plus - I_exp))
-    minus = 2*0.051**2*(2.4095*9.8*(slope - std_err)-2.4095/2)
-    print('Error of I in exp = {}'.format(I_exp - minus))
-    print('I = {} +- {}'.format(I_exp, plus - I_exp))
-plt.show()
+    breaking_point = input('Do you need Breaking point?(yes or no):')
+    
+    if breaking_point == 'yes':
+        breaking_point = eval(input('Which one?(e.g.15. This means you wanna split data points into 0-14 and 14 to after):'))
+   
+        x_name = df.columns[0]
+        y_name = df.columns[1]
+        x_values = list(df[x_name].values)
+        y_values = list(df[y_name].values)
+        slope_1, intercept_1, r_value_1, p_value_1, std_err_1 = linregress(x_values[0:breaking_point], y_values[0:breaking_point])
+        slope_2, intercept_2, r_value_2, p_value_2, std_err_2 = linregress(x_values[breaking_point:-1], y_values[breaking_point:-1])
+        #Fit x and y Data using a linear regression
+        xfit_1 = np.linspace(x_values[0], x_values[-1], 100)
+        xfit_2 = np.linspace(x_values[0], x_values[-1], 100)
+        cross_zero = input('Do you need intercept to be zero(yes or no):')
+        if cross_zero == 'yes':
+            intercept = 0
+        else:
+            pass
+        yfit_1 = slope_1*xfit_1 + intercept_1
+        yfit_2 = slope_2*xfit_2 + intercept_2
+        plt.plot(xfit_1, yfit_1, 'r-', label= 'Linear Fit_1')
+        plt.plot(xfit_2, yfit_2, 'g-', label= 'Linear Fit_2')
+        plt.errorbar(x_values[0:breaking_point],y_values[0:breaking_point],fmt='o',label='Data point_1', xerr = 0, yerr = std_err_1, capsize = 5)
+        plt.errorbar(x_values[breaking_point:-1],y_values[breaking_point:-1],fmt='o',label='Data point_2', xerr = 0, yerr = std_err_2, capsize = 5)
+        '''
+        we don't want error on x axis
+        to make vertical line longer, we need to set parameter 'capsize' with appropriate value
+        '''
+        intersection_x = (intercept_2 - intercept_1)/(slope_1 - slope_2)
+        intersection_y = (slope_1 * intersection_x + intercept_1)
+        inters_y_mdf = intersection_y * 0.996
+        plt.plot(intersection_x, intersection_y, 'bo', label = 'Intersection')
+        plt.text(intersection_x, inters_y_mdf, '(' + str(round(intersection_x,2)) + ',' + str(round(intersection_y,2)) + ')')
+        
+        Unit_x = input('Please input unit of X axis(e.g.[m]):')
+        Unit_y = input('Please input unit of Y axis(e.g.[m]):')
+        print('-----------------------------------\n')
+        print('Slope_1, Error_1, Intercept_1, Correlation Coefficient_1:') 
+        # r^2 = coefficient of determination
+        print(slope_1, std_err_1, intercept_1, r_value_1**2)
+        print('-----------------------------------\n')
+        print('Slope_2, Error_2, Intercept_2, Correlation Coefficient_2:')
+        print(slope_2, std_err_2, intercept_2, r_value_2**2)
+        print('-----------------------------------')
+        
+        plt.grid()
+        plt.xlabel(x_name + Unit_x)
+        plt.ylabel(y_name + Unit_y)
+        plt.legend(loc = 'best')
+        plt.show()
+        
+    elif breaking_point == 'no':
+        x_name = df.columns[0]
+        y_name = df.columns[1]
+        x_values = list(df[x_name].values)
+        y_values = list(df[y_name].values)
+        slope_1, intercept_1, r_value_1, p_value_1, std_err_1 = linregress(x_values, y_values)
+        xfit_1 = np.linspace(x_values[0], x_values[-1], 100)
+        cross_zero = input('Do you need intercept to be zero(yes or no):')
+        if cross_zero == 'yes':
+            intercept = 0
+        else:
+            pass
+        yfit_1 = slope_1*xfit_1 + intercept_1
+        plt.plot(xfit_1, yfit_1, 'r-', label= 'Linear Fit_1')
+        plt.errorbar(x_values, y_values, fmt='o',label='Data point_1', xerr = 0, yerr = std_err_1, capsize = 5)
+        # we don't want error on x axis
+        # to make vertical line longer, we need to set parameter 'capsize' with appropriate value
+        Unit_x = input('Please input unit of X axis(e.g.[m]):')
+        Unit_y = input('Please input unit of Y axis(e.g.[m]):')
+        print('-----------------------------------\n')
+        print('Slope_1, Error_1, Intercept_1, Correlation Coefficient_1:') 
+        # r^2 = coefficient of determination
+        print(slope_1, std_err_1, intercept_1, r_value_1**2)
+        print('-----------------------------------\n')
+        
+        plt.grid()
+        plt.xlabel(x_name + Unit_x)
+        plt.ylabel(y_name + Unit_y)
+        plt.legend(loc = 'best')
+        plt.show()
+        
+    else:
+        print('Invalid input')
+        
+   
+    
+if __name__ == '__main__':
+    main()
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
